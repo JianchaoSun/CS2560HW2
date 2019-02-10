e866ac4cca6a1ba81ecfa678357928d2f75e06b8
JianchaoSun <39354423+JianchaoSun@users.noreply.github.com>
2019-02-10 13:48:31 -0800

    Update patternDisplay.c

--- homework2_4/patternDisplay.c
+++ homework2_4/patternDisplay.c
@@ -10,14 +10,14 @@ int main(){
 	int i =1;
 	for(i =1;i<=10;i++){
 		for(int j = 0; j <i ; j++){
-			printf("*");
+			printf("+");
 		}
 		printf("\n");
 	}
 
 	for(i=10; i>0; i--){
 		for(int j=0; j <i; j++){
-			printf("*");
+			printf("+");
 		}
 		printf("\n");
 	}
